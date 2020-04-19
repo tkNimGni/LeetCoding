1d36f17c0d2f43449408c6be56bbe0de20f509cc
ryou <ryou90723@gmail.com>
2020-04-19 17:27:32 +0800

    033 ac O(logn)

--- LeetCode033.js
+++ LeetCode033.js
@@ -4,8 +4,17 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for (let i in nums) {
-        if (nums[i] === target) return i
+    let l = 0, r = nums.length - 1
+    while (l <= r) {
+        let m = Math.floor(l + (r - l) / 2)
+        if (nums[m] === target) return m
+        if (nums[m] >= nums[l]) {
+            if (nums[l] <= target && nums[m] > target) r = m - 1
+            else l = m + 1
+        } else {
+            if (nums[m] < target && nums[r] >= target) l = m + 1
+            else r = m - 1
+        }
     }
     return -1
 };
