5e5e5503b11e698697a71e04b3dc4f9c8e48a23f
ryou <ryou90723@gmail.com>
2020-04-26 17:46:55 +0800

    1143 ac

--- LeetCode1143.js
+++ LeetCode1143.js
@@ -7,12 +7,12 @@ var longestCommonSubsequence = function(text1, text2) {
     if (text1 === text2) return text1.length
     let dp = new Array(text2.length + 1).fill(0)
     for (let i in text1) {
-        let temp = [...dp]
+        let prev = 0
         for (let j = 1; j <= text2.length; j++) {
-            if (text1[i] === text2[j - 1]) temp[j] = dp[j - 1] + 1
-            else temp[j] = Math.max(dp[j], temp[j - 1])
+            let temp = dp[j]
+            dp[j] = text1[i] === text2[j - 1] ? prev + 1 : Math.max(dp[j], dp[j - 1])
+            prev = temp
         }
-        dp = temp
     }
     return dp[text2.length]
 };
@@ -20,8 +20,8 @@ var longestCommonSubsequence = function(text1, text2) {
 // let testCase = ['c', 'j']
 // let testCase = ['abcde', 'ace']
 // let testCase = ['bdcaba', 'abcbda']
-let testCase = ['pmjghexybyrgzczy', 'hafcdqbgncrcbihkd']
-// let testCase = ['bsbininm', 'jmjkbkjkv']
+// let testCase = ['pmjghexybyrgzczy', 'hafcdqbgncrcbihkd']
+let testCase = ['bsbininm', 'jmjkbkjkv']
 console.log('Input', testCase)
 console.time()
 let output = longestCommonSubsequence(testCase[0],testCase[1])
