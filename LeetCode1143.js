d20f606e54f73dad8b6b8f784122e083fe9b0649
ryou <ryou90723@gmail.com>
2020-04-26 17:30:11 +0800

    1143 ac

--- LeetCode1143.js
+++ LeetCode1143.js
@@ -4,14 +4,13 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    let dp = []
-    for (let j = 0; j <= text2.length; j++) {
-        dp[j] = 0
-    }
+    if (text1 === text2) return text1.length
+    let dp = new Array(text2.length + 1).fill(0)
     for (let i in text1) {
-        let temp = JSON.parse(JSON.stringify(dp))
+        let temp = [...dp]
         for (let j = 1; j <= text2.length; j++) {
-            temp[j] = text1[i] === text2[j - 1] ? dp[j - 1] + 1 : Math.max(dp[j], temp[j - 1])
+            if (text1[i] === text2[j - 1]) temp[j] = dp[j - 1] + 1
+            else temp[j] = Math.max(dp[j], temp[j - 1])
         }
         dp = temp
     }
@@ -22,6 +21,7 @@ var longestCommonSubsequence = function(text1, text2) {
 // let testCase = ['abcde', 'ace']
 // let testCase = ['bdcaba', 'abcbda']
 let testCase = ['pmjghexybyrgzczy', 'hafcdqbgncrcbihkd']
+// let testCase = ['bsbininm', 'jmjkbkjkv']
 console.log('Input', testCase)
 console.time()
 let output = longestCommonSubsequence(testCase[0],testCase[1])
