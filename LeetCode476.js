b35ff856994f9c31616c64f073a0d8974eaaeb87
ryou <ryou90723@gmail.com>
2020-05-04 15:44:00 +0800

    476 ac

---
+++ LeetCode476.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    let mask = 1
+    while (mask < num) mask = (mask << 1) | 1
+    return num ^ mask
+};
