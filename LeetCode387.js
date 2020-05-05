da2afd9d1272ae873b8d4b9f85d3862515a8c046
wryou <wryou@toydea.com>
2020-05-05 17:17:35 +0900

    387 ac

--- LeetCode387.js
+++ LeetCode387.js
@@ -3,15 +3,17 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let hash = new Map()
+    let map = {}
 
     for (let a of s) {
-        hash.set(a, hash.has(a) ? hash.get(a) + 1 : 1)
+        map[a] = map[a] ? map[a] + 1 : 1
     }
 
     for (let i in s) {
-        if (hash.get(s[i]) === 1) return +i
+        if (map[s[i]] === 1) return +i
     }
 
     return -1
 };
+
+console.log(firstUniqChar('abab'))
