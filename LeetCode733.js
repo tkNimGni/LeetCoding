9d3c13f938cb2537d562a72e0bdadd9281c7b857
ryou <ryou90723@gmail.com>
2020-05-11 22:43:59 +0800

    773 ac

--- LeetCode733.js
+++ LeetCode733.js
@@ -7,11 +7,11 @@
  */
 var floodFill = function(image, sr, sc, newColor) {
     const color = image[sr][sc]
-    let check = new Array(image.length).fill(new Array(image[0].length).fill(false))
+    let check = new Map()
     const floodFillRecursive = (sr, sc) => {
-        if (sr >= 0 && sr < image.length && sc >= 0 && sc <= image[0].length && image[sr][sc] === color && !check[sr][sc]) {
+        if (sr >= 0 && sr < image.length && sc >= 0 && sc <= image[0].length && image[sr][sc] === color && !check.has(sr.toString() + ',' + sc.toString())) {
             image[sr][sc] = newColor
-            check[sr][sc] = true
+            check.set(sr.toString() + ',' + sc.toString(), newColor)
             floodFillRecursive(sr - 1, sc)
             floodFillRecursive(sr + 1, sc)
             floodFillRecursive(sr, sc - 1)
@@ -26,7 +26,7 @@ const print = (image) => {
     return image.join('\n')
 }
 
-// testCase = [[[1,2,1],[1,1,0],[1,0,1]], 1, 1, 2]
-testCase = [[[0,0,0],[0,1,1]], 1, 1, 1]
+testCase = [[[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2]
+// testCase = [[[0,0,0],[0,1,1]], 1, 1, 1]
 console.log(print(testCase[0]) + '\n')
 console.log(print(floodFill(testCase[0], testCase[1], testCase[2], testCase[3])))
