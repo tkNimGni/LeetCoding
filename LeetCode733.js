679522c47bfe61170018ebab4643b54de8404497
ryou <ryou90723@gmail.com>
2020-05-11 22:30:17 +0800

    773 wa

--- LeetCode733.js
+++ LeetCode733.js
@@ -7,9 +7,11 @@
  */
 var floodFill = function(image, sr, sc, newColor) {
     const color = image[sr][sc]
+    let check = new Array(image.length).fill(new Array(image[0].length).fill(false))
     const floodFillRecursive = (sr, sc) => {
-        if (sr >= 0 && sr < image.length && sc >= 0 && sc <= image[0].length && image[sr][sc] === color) {
+        if (sr >= 0 && sr < image.length && sc >= 0 && sc <= image[0].length && image[sr][sc] === color && !check[sr][sc]) {
             image[sr][sc] = newColor
+            check[sr][sc] = true
             floodFillRecursive(sr - 1, sc)
             floodFillRecursive(sr + 1, sc)
             floodFillRecursive(sr, sc - 1)
@@ -24,6 +26,7 @@ const print = (image) => {
     return image.join('\n')
 }
 
-testCase = [[[1,2,1],[1,1,0],[1,0,1]], 1, 1, 2]
+// testCase = [[[1,2,1],[1,1,0],[1,0,1]], 1, 1, 2]
+testCase = [[[0,0,0],[0,1,1]], 1, 1, 1]
 console.log(print(testCase[0]) + '\n')
 console.log(print(floodFill(testCase[0], testCase[1], testCase[2], testCase[3])))
