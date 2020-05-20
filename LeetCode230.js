77b0c7e4f422845ffea0416b0746d35c706a85d7
ryou <ryou90723@gmail.com>
2020-05-20 23:17:19 +0800

    230 wa

---
+++ LeetCode230.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+    let arr = []
+
+    const traversal = (root) => {
+        if (!root) return
+        if (root.left) traversal(root.left)
+        if (root.val) arr.push(root.val)
+        if (root.right) traversal(root.right)
+    }
+
+    traversal(root)
+
+    return arr[k - 1]
+}
