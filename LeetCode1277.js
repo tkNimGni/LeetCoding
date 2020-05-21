a4a2428e8bf02550eb4852ed6316a566e2940d6e
ryou <ryou90723@gmail.com>
2020-05-21 23:41:41 +0800

    1277 ac

--- LeetCode1277.js
+++ LeetCode1277.js
@@ -7,7 +7,7 @@ var countSquares = function (matrix) {
     const n = matrix[0].length
     let ans = 0
     for (let i = 0; i < m; i++) ans += matrix[i][0]
-    for (let i = 0; i < n; i++) ans += matrix[0][i]
+    for (let i = 1; i < n; i++) ans += matrix[0][i]
     for (let i = 1; i < m; i++) {
         for (let j = 1; j < n; j++) {
             matrix[i][j] += matrix[i][j]
