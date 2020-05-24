34c17d0fe6376ae2178fc0c52d3fa40850d93c6f
ryou <ryou90723@gmail.com>
2020-05-24 15:24:39 +0800

    1008 ac

--- LeetCode1008.js
+++ LeetCode1008.js
@@ -9,37 +9,41 @@
  * @param {number[]} preorder
  * @return {TreeNode}
  */
-var bstFromPreorder = function(preorder) {
+var bstFromPreorder = function (preorder) {
     if (!preorder.length) return null
-    
+
     const [root, ...rest] = preorder
-    
+
     const rootNode = new TreeNode(root)
-    rootNode.left = bstFromPreorder(rest.filter(n => n < root))
-    rootNode.right = bstFromPreorder(rest.filter(n => n > root))
-    
+    rootNode.left = bstFromPreorder(rest.filter((n) => n < root))
+    rootNode.right = bstFromPreorder(rest.filter((n) => n > root))
+
     return rootNode
-};
+}
 
 class TreeNode {
     constructor(val) {
-        this.val = val;
-        this.left = this.right = null;
+        this.val = val
+        this.left = this.right = null
     }
 }
 
-let arr = []
 function PreorderTraversal(bst) {
-    if (bst) {
-        arr.push(bst.val)
-        PreorderTraversal(bst.left)
-        PreorderTraversal(bst.right)
-        return arr
+    let arr = []
+    const traversal = (bst) => {
+        if (bst) {
+            arr.push(bst.val)
+            traversal(bst.left)
+            traversal(bst.right)
+        }
     }
+    traversal(bst)
+    return arr
 }
 
+const testCase = [8, 5, 1, 7, 10, 12]
+console.log('Input', testCase)
 console.time()
-let testCase = [8,5,1,7,10,12]
-console.log("Input", testCase)
-console.log("Output", PreorderTraversal(bstFromPreorder(testCase)))
+const output = bstFromPreorder(testCase)
 console.timeEnd()
+console.log('Output', PreorderTraversal(output))
