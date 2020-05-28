fa6f40c212498832d307156b996f16f0ea9dacaa
ryou <ryou90723@gmail.com>
2020-05-28 22:45:26 +0800

    338 ac

--- LeetCode338.js
+++ LeetCode338.js
@@ -4,10 +4,6 @@
  */
 var countBits = function (num) {
     let ans = [0]
-    if (num === 0) return ans
-    for (let n = (pre = 1); n <= num; n++) {
-        if (n === pre << 1) pre = n
-        ans.push(1 + ans[n - pre])
-    }
+    for (let i = 1; i <= num; i++) ans.push(ans[i >> 1] + (i & 1))
     return ans
 }
