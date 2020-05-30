aa12c42c59d6b9676a693056a1b8f91d96e495ec
ryou <ryou90723@gmail.com>
2020-05-30 16:54:22 +0800

    973 ac

--- LeetCode973.js
+++ LeetCode973.js
@@ -5,14 +5,8 @@
  */
 var kClosest = function (points, K) {
     return points
-        .map((point) => {
-            return { point: point, dis: point[0] ** 2 + point[1] ** 2 }
-        })
         .sort((a, b) => {
-            return a.dis - b.dis
+            return a[0] ** 2 + a[1] ** 2 - (b[0] ** 2 + b[1] ** 2)
         })
         .slice(0, K)
-        .map((point) => {
-            return point.point
-        })
 }
