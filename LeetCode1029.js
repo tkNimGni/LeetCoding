7850165642551aa8cee8f517d63d56a268351da7
ryou <ryou90723@gmail.com>
2020-06-04 00:43:13 +0800

    1029 wa

--- LeetCode1029.js
+++ LeetCode1029.js
@@ -11,51 +11,56 @@ var twoCitySchedCost = function (costs) {
     }
     if (cityA === cityB) return ans
 
-    costs = costs.sort((cityA, cityB) => {
-        return Math.abs(cityA[0] - cityA[1]) - Math.abs(cityB[0] - cityB[1])
+    costs = costs.sort((costA, costB) => {
+        return Math.abs(costA[0] - costA[1]) - Math.abs(costB[0] - costB[1])
     })
-
     for (let i = 0; cityA !== cityB && i < costs.length; i++) {
         const costA = costs[i][0]
         const costB = costs[i][1]
-        if (costA === costB) continue
-        if (cityA > cityB && costA < costB) {
-            ans += costB - costA
-            cityA--
-            cityB++
-        } else if (cityA < cityB && costA > costB) {
-            ans += costA - costB
-            cityA++
-            cityB--
-        }
+        if ((cityA > cityB && costA > costB) || (cityA < cityB && costA < costB)) continue
+        ans += (costA - costB) * (cityA > cityB ? -1 : 1)
+        cityA += cityA > cityB ? -1 : 1
+        cityB += cityA > cityB ? 1 : -1
     }
     return ans
 }
 
+// let testCase = [
+//     [841, 185],
+//     [295, 946],
+//     [928, 450],
+//     [55, 806],
+//     [714, 89],
+//     [787, 380],
+//     [663, 323],
+//     [814, 265],
+//     [581, 581],
+//     [850, 486],
+//     [390, 491],
+//     [560, 678],
+//     [311, 283],
+//     [145, 772],
+//     [987, 471],
+//     [465, 611],
+//     [926, 367],
+//     [782, 532],
+//     [299, 317],
+//     [605, 260],
+//     [751, 735],
+//     [614, 746],
+//     [747, 904],
+//     [267, 653],
+// ]
 let testCase = [
-    [841, 185],
-    [295, 946],
-    [928, 450],
-    [55, 806],
-    [714, 89],
-    [787, 380],
-    [663, 323],
-    [814, 265],
-    [581, 581],
-    [850, 486],
-    [390, 491],
-    [560, 678],
-    [311, 283],
-    [145, 772],
-    [987, 471],
-    [465, 611],
-    [926, 367],
-    [782, 532],
-    [299, 317],
-    [605, 260],
-    [751, 735],
-    [614, 746],
-    [747, 904],
-    [267, 653],
+    [518, 518],
+    [71, 971],
+    [121, 862],
+    [967, 607],
+    [138, 754],
+    [513, 337],
+    [499, 873],
+    [337, 387],
+    [647, 917],
+    [76, 417],
 ]
 console.log(twoCitySchedCost(testCase))
