ebe484776202804bcba866719f3d5cefa8059ce1
G.L. Wang <ryou90723@gmail.com>
2022-03-13 03:34:08 +0800

    138 re

---
+++ LeetCode138.cs
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+    
+    public Node(int _val) {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+*/
+
+public class Solution {
+    private Dictionary<Node, Node> dic = new Dictionary<Node, Node>();
+    
+    public Node CopyRandomList(Node head) {
+        var newHead = new Node(head.val);
+        dic[head] = newHead;
+        
+        if (head.next != null) {
+            newHead.next = CopyRandomList(head.next);
+        }
+        
+        if (head.random != null) {
+            newHead.random = dic.ContainsKey(head.random) ? dic[head.random] : CopyRandomList(head.random);
+        }
+        
+        return newHead;
+    }
+}
