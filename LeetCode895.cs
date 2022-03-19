fa29aa937bc477faf354e2e39e0e14f12bcb4c7f
tkNimGni <ryou90723@gmail.com>
2022-03-20 02:31:23 +0800

    895 ac

--- LeetCode895.cs
+++ LeetCode895.cs
@@ -1,16 +1,22 @@
 public class FreqStack {
-    Dictionary<int, int> freq = new Dictionary<int, int>();
-    Dictionary<int, Stack<int>> freqStack = new Dictionary<int, Stack<int>>();
+    Dictionary<int, int> freq = new Dictionary<int, int>(); // value -> frequency
+    Dictionary<int, Stack<int>> freqStack = new Dictionary<int, Stack<int>>(); // frequency -> stack of values
     int maxFreq = 0;
     
     public FreqStack() { }
     
     public void Push(int val) {
-
+        freq[val] = freq.ContainsKey(val) ? freq[val] + 1 : 1;
+        maxFreq = Math.Max(maxFreq, freq[val]);
+        if (!freqStack.ContainsKey(freq[val])) freqStack[freq[val]] = new Stack<int>();
+        freqStack[freq[val]].Push(val);
     }
 
     public int Pop() {
-        
+        int val = freqStack[maxFreq].Pop();
+        freq[val]--;
+        if (freqStack[maxFreq].Count == 0) maxFreq--;
+        return val;
     }
 }
 
