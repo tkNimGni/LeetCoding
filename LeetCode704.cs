af0b99e1e2fd1ffa2fe27825d9d6b457732b517c
tkNimGni <ryou90723@gmail.com>
2022-03-26 13:15:01 +0800

    704 wa

--- LeetCode704.cs
+++ LeetCode704.cs
@@ -1,5 +1,9 @@
 public class Solution {
     public int Search(int[] nums, int target) {
+        if (nums.Length == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
         var start = 0;
         var end = nums.Length - 1;
         while (start < end) {
