fac7d1618f618120cee5718c066dc3cbb42ad37d
tkNimGni <ryou90723@gmail.com>
2022-04-07 02:32:26 +0800

    923 ac

--- LeetCode923.cs
+++ LeetCode923.cs
@@ -1,42 +1,42 @@
 public class Solution {
     public int ThreeSumMulti(int[] arr, int target) {
         var mod = 1000000007;
-        var dic = new Dictionary<int, int>();
-        var list = new List<int>();
+        var countDic = new Dictionary<int, int>();
+        var keyList = new List<int>();
+        var indexDic = new Dictionary<int, int>();
         long ans = 0;
         
         for (var i = 0; i < arr.Length; i++) {
-            if (dic.ContainsKey(arr[i])) {
-                dic[arr[i]]++;
+            if (countDic.ContainsKey(arr[i])) {
+                countDic[arr[i]]++;
             } else {
-                dic[arr[i]] = 1;
-                list.Add(arr[i]);
+                countDic[arr[i]] = 1;
+                keyList.Add(arr[i]);
+                indexDic.Add(arr[i], keyList.Count() - 1);
             }
         }
 
-        list.Sort((a, b) => a - b);
-
-        for (var i = 0; i < list.Count(); i++) {
-            long n = dic[list[i]];
+        for (var i = 0; i < keyList.Count(); i++) {
+            long n = countDic[keyList[i]];
             // 3 nums duplicated
-            if (dic[list[i]] >= 3 && list[i] * 3 == target) {
+            if (countDic[keyList[i]] >= 3 && keyList[i] * 3 == target) {
                 ans += n * (n - 1) * (n - 2) / 6;
                 // Console.WriteLine($"({list[i]},{list[i]},{list[i]}) * {dic[list[i]]}");
             }
 
             // 2 nums duplicated
-            var twoDuplicatedRest = target - list[i] * 2;
-            if (n >= 2 && twoDuplicatedRest != list[i] && dic.ContainsKey(twoDuplicatedRest)) {
-                var sum = (n * (n - 1) / 2) * dic[twoDuplicatedRest];
+            var twoDuplicatedRest = target - keyList[i] * 2;
+            if (n >= 2 && twoDuplicatedRest != keyList[i] && countDic.ContainsKey(twoDuplicatedRest)) {
+                var sum = (n * (n - 1) / 2) * countDic[twoDuplicatedRest];
                 ans += sum;
                 // Console.WriteLine($"({list[i]},{list[i]},{twoDuplicatedRest}) * {sum}");
             }
 
             // 3 nums distincted
-            for (var j = i + 1; j < list.Count(); j++) {
-                var distinctRest = target - list[i] - list[j];
-                if (distinctRest > list[j] && dic.ContainsKey(distinctRest)) {
-                    var sum = dic[list[i]] * dic[list[j]] * dic[distinctRest];
+            for (var j = i + 1; j < keyList.Count(); j++) {
+                var distinctRest = target - keyList[i] - keyList[j];
+                if (countDic.ContainsKey(distinctRest) && indexDic[distinctRest] > j) {
+                    var sum = countDic[keyList[i]] * countDic[keyList[j]] * countDic[distinctRest];
                     ans += sum;
                     // Console.WriteLine($"({list[i]},{list[j]},{distinctRest}) * {sum}");
                 }
