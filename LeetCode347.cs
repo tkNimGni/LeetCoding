bcfa5f42ff005d191783a94c9d4958482f6ab269
tkNimGni <ryou90723@gmail.com>
2022-04-10 17:25:36 +0800

    347 wa

---
+++ LeetCode347.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        var numCount = new Dictionary<int, int>();
+        var countNums = new Dictionary<int, HashSet<int>>();
+
+        foreach (var n in nums) {
+            if (numCount.ContainsKey(n)) {
+                numCount[n]++;
+            } else {
+                numCount[n] = 1;
+            }
+
+            var count = numCount[n];
+            if (countNums.ContainsKey(count)) {
+                countNums[count].Add(n);
+            } else {
+                countNums[count] = new HashSet<int>() { n };
+            }
+            if (count > 1) {
+                countNums[count - 1].Remove(n);
+            }
+        }
+        
+        var ans = new List<int>();
+        var keys = countNums.Keys.ToList();
+        keys.Reverse();
+        foreach (var key in keys) {
+            while (ans.Count < k) {
+                ans.AddRange(countNums[key]);
+            }
+            if (ans.Count >= k) {
+                break;
+            }
+        }
+
+        return ans.ToArray();
+    }
+}
